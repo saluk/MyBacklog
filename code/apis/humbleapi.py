a8a2d4a4b5cf163769f1965467557d640998f2ff
Patrick Mullen <saluk64007@gmail.com>
2015-05-12 21:12:00 -0700

    Update gog and humble for importing

--- code/apis/humbleapi.py
+++ code/apis/humbleapi.py
@@ -2,21 +2,20 @@
 import time
 import requests
 import re
-import data
+from code import data
 import json
 
 class Browser:
     def __init__(self):
         self.cookies = {}
-        #self.cookies = {"guc_al":"0","sessions_gog_com":"0","__utma":"95732803.1911890316.1399672018.1399672018.1399672018.1","__utmb":"95732803.5.9.1399672201295","__utmz":"95732803.1399672018.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)"}
         self.headers = {"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Encoding":"gzip, deflate","Accept-Language":"en-us,ko;q=0.7,en;q=0.3",
             "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:28.0) Gecko/20100101 Firefox/28.0",
             "Referer":"https://www.humblebundle.com/",
             "X-Requested-With":"XMLHttpRequest"}
-    def post(self,url,data={},params={}):
+    def post(self,url,datax={},params={}):
         self.json = {}
         self.text = ""
-        answer = requests.post(url,data=data,params=params,cookies=self.cookies,headers=self.headers,allow_redirects=True)
+        answer = requests.post(url,data=datax,params=params,cookies=self.cookies,headers=self.headers,allow_redirects=True)
         self.answer = answer
         self.cookies.update(answer.cookies)
         try:
@@ -48,10 +47,21 @@ import time
 def get_humble_gamelist():
     b = Browser()
     logged_in = False
+    b.get("https://www.humblebundle.com/")
     if not logged_in:
-        b.get("https://www.humblebundle.com")
+        try:
+            f = open("cache/hcookies","r")
+            b.cookies = eval(f.read())
+            f.close()
+        except:
+            pass
+        b.get("https://www.humblebundle.com/home")
+        if "error_id" not in b.text:
+            logged_in = True
+            print("used cookies!")
+    if not logged_in:
         print("b.cookies:",b.cookies)
-        b.post("https://www.humblebundle.com/login",{
+        b.post("https://www.humblebundle.com/processlogin",{
             "authy-token":"",
             "_le_csrf_token":b.cookies["csrf_cookie"],
             "goto":"/home",
@@ -61,13 +71,22 @@ def get_humble_gamelist():
             "username":"saluk64007@gmail.com"})
         print ("loggedin",b.url)
         print (b.cookies)
+        f = open("humble.html","w")
+        f.write(b.text)
+        f.close()
+
+    f = open("cache/hcookies","w")
+    f.write(repr(b.cookies))
+    f.close()
     
     #Should be logged in now
     api_get_order = "https://www.humblebundle.com/api/v1/order/%(key)s"
     #print (b.url,b.text)
     b.get("https://www.humblebundle.com/home")
-    print(b.text)
-    keys = re.findall("gamekeys\:.*?\[(.*?)\]",b.text)[0]
+    f = open("humble2.html","w")
+    f.write(b.text)
+    f.close()
+    keys = re.findall("gamekeys \=.*?\[(.*?)\]",b.text)[0]
     print (keys)
     games = []
     for key in keys.split(","):
@@ -76,18 +95,14 @@ def get_humble_gamelist():
         print (b.json)
         hdata = b.json
         package = data.Game(name=hdata["product"]["human_name"],
-                                        humble_package=hdata["gamekey"],
-                                        humble_machinename=hdata["product"]["machine_name"],
-                                        source="humble",
                                         is_package=1)
+        package.sources = [{"source":"humble","id":hdata["product"]["machine_name"],"package":hdata["gamekey"]}]
         games.append(package)
         for sub in hdata["subproducts"]:
             game = data.Game(name=sub["human_name"],
-                                        source="humble",
-                                        humble_machinename=sub["machine_name"],
-                                        humble_package=hdata["gamekey"],
                                         website=sub["url"],
                                         icon_url=sub["icon"])
+            game.sources = [{"source":"humble","id":sub["machine_name"],"package":hdata["gamekey"]}]
             games.append(game)
     for g in games:
         print (g.name,g.icon_url)
