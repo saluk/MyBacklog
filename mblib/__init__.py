374ca6eb7b2d8043d16033b57f47b920eb3e76e2
saluk <saluk@localhost>
2019-04-25 00:15:10 -0700

    Update version display, don't sync while game is still running

---
+++ mblib/__init__.py
@@ -0,0 +1,3 @@
+VERSION = "0.5"
+last_revision_received = -1
+last_revision_sent = -1
--- mblib/sync.py
+++ mblib/sync.py
@@ -1,16 +1,21 @@
 import requests
 import shutil, os
 
+import mblib
 from mblib import games
 
 app = None
 user = "saluk"
-host = "mybacklog.dawnsoft.org:8000"
+host = "mybacklog.tinycrease.com:5000"
 
+mblib.last_revision_sent = -1
+mblib.last_revision_received = -1
+mblib.last_revision_loaded = -1
 
 def get_server_revision():
     try:
-        r = requests.get("http://%s/games_revision?user=%s"%(host,user),timeout=2)
+        r = requests.get("http://%s/games_revision?user=%s" %
+                         (host, user), timeout=2)
         return r.json()["server_revision"]
     except:
         print("COULDN'T ACCESS SERVER")
@@ -40,16 +45,17 @@ def refresh_games():
     downloading = game_file+".d"
     downloaded = games.Games()
     downloaded.load(downloading)
-    #force update all games from server into games
+    # force update all games from server into games
     for g in downloaded.games.values():
-        gamedb.force_update_game(g.gameid,g)
-    #Delete games that are gone on server
+        gamedb.force_update_game(g.gameid, g)
+    # Delete games that are gone on server
     for gameid in list(gamedb.games.keys()):
         if gameid in gamedb.games and gameid not in downloaded.games:
             print("DELETING",gamedb.games[gameid])
             del gamedb.games[gameid]
-    #Update local revision to match server
+    # Update local revision to match server
     gamedb.revision = downloaded.revision
+
 
 def download():
     print("check to download")
@@ -58,27 +64,32 @@ def download():
         return False
     games = app.games
     revision = get_server_revision()
+    mblib.last_revision_received = revision
     if not revision:
         print(" no db on server")
+        raise Exception("SERVER NOT RUNNING")
         return False
+    print("SERVER VERSION", revision)
     if revision < games.revision:
         print(" MAJOR ERROR, server is older than client. shouldn't happen, but we are uploading the version")
         upload()
         return False
     if revision == games.revision:
-        #We are already in sync
+        # We are already in sync
         print(" we are good")
         return False
     download_games()
     refresh_games()
     return True
-    
+
+
 def upload():
     print("UPLOAD GAMES")
+    mblib.last_revision_sent = app.games.revision
     game_file = app.config["games"]
-    with open(game_file,"rb") as f:
+    with open(game_file, "rb") as f:
         data = f.read()
     r = requests.put("http://%s/game_database?user=%s"%(host,user),data=data,headers={"Content-Type":"application/ubjson"})
     print(r.json())
-    if r.json().get("error",""):
+    if r.json().get("error", ""):
         raise Exception(r.text)
